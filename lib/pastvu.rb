82d5fe25822fe26066bd577bc7ea6c1211a864be
projecteurlumiere <129510705+projecteurlumiere@users.noreply.github.com>
2023-12-23 13:36:12 +0300

    initial commit

---
+++ lib/pastvu.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "pastvu/version"
+
+module Pastvu
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/pastvu/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Pastvu
+  VERSION = "0.1.0"
+end
