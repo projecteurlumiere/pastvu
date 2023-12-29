52e8fa315ee12c3b92bb1fc465a3f19a6e7e5964
projecteurlumiere <129510705+projecteurlumiere@users.noreply.github.com>
2023-12-29 18:41:07 +0300

    implement Photo class

--- lib/pastvu/photo.rb
+++ lib/pastvu/photo.rb
@@ -1,13 +1,40 @@
+require "open-uri"
+
 module Pastvu
   class Photo
     # VALID_ATTRIBUTES = %w[cid s file title dir geo year ccount]
     # attr_accessor *VALID_ATTRIBUTES
 
-    def initialize(params = {})
-      params.each do |key, value|
+    def initialize(attributes = {})
+      attributes.each do |key, value|
         instance_variable_set("@#{key}", value)
         self.class.send(:attr_accessor, key)
       end
+    end
+
+    def download(size, path)
+      raise ArgumentError, "expect size to be correct symbol" unless %i[original standard thumbnail thumb].include?(size)
+      raise ArgumentError, "expect file extension to be .jpeg or .jpg" unless path[-4..-1] == ".jpg" || path[-5..-1] == ".jpeg"
+
+      download = URI.parse(method(size).call).open
+      IO.copy_stream(download, path)
+      File.new(path)
+    end
+
+    def standard
+      "https://pastvu.com/_p/d/".concat(file)
+    end
+
+    def original
+      "https://pastvu.com/_p/a/".concat(file)
+    end
+
+    def thumbnail
+      "https://pastvu.com/_p/h/".concat(file)
+    end
+
+    def thumb
+      thumbnail
     end
   end
 end
