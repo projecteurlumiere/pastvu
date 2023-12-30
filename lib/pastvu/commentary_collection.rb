c09a9fc56e3fc5cf2466cda14678c824b217a77e
projecteurlumiere <129510705+projecteurlumiere@users.noreply.github.com>
2023-12-30 14:14:20 +0300

    refactor a bit collection classes

--- lib/pastvu/commentary_collection.rb
+++ lib/pastvu/commentary_collection.rb
@@ -2,18 +2,15 @@ module Pastvu
   class CommentaryCollection < BasicResponse
     include Enumerable
 
-    def initialize(response_body)
-      super response_body
-      @hash ||= self.to_hash
-    end
-
     def each
+      @hash ||= self.to_hash
       @hash["result"]["comments"].each do |comment_hash|
         yield Commentary.new comment_hash
       end
     end
 
     def users
+      @hash ||= self.to_hash
       @hash["result"]["users"]
     end
   end
--- lib/pastvu/photo_collection.rb
+++ lib/pastvu/photo_collection.rb
@@ -2,12 +2,8 @@ module Pastvu
   class PhotoCollection < BasicResponse
     include Enumerable
 
-    def initialize(response_body)
-      super response_body
-      @hash ||= self.to_hash
-    end
-
     def each
+      @hash ||= self.to_hash
       @hash["result"]["photos"].each do |photo_hash|
         yield Photo.new photo_hash
       end
